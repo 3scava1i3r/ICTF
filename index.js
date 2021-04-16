e080d145ba16ec55aadcd23e2f745ceca6ddda6c
Hritwik Tripathi <Tripathi.hritwik@gmail.com>
2021-04-16 22:49:31 +0530

    new stuff

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Client, RichEmbed } = require("discord.js");
 const bot = new Client();
 
-const token = "ODMyNjIzMDgwMTgyNTEzNzI1.YHmeqw.26-K8nKMEi30y9Vy4v4VZjMdEKc";
+const token = "ODMyNjIzMDgwMTgyNTEzNzI1.YHmeqw.XpBjrVhsQfuabNXkQqWrvPspltk";
 
 const PREFIX = "=";
 
@@ -14,16 +14,14 @@ bot.on("message", (message) => {
 
   switch (args[0]) {
     case "flag":
-      const Embed = new RichEmbed()
-        .setTitle("Helper Embed")
-        .setColor(0xff0000)
-        .setDescription(
-          "ICTF{Fl4g_F0und_0n_Di5c0rd}"
-        );
+      
 
-      message.author.send(Embed);
+      message.author.send("ICTF{Fl4g_F0und_0n_Di5c0rd}");
       break;
   }
 });
 
 bot.login(token);
+
+
+// ICTF{Fl4g_F0und_0n_Di5c0rd}
